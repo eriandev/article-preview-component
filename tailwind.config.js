f09fd3883a7b24ea272f572655730041f9512420
erianvc <erianvc.dev@gmail.com>
2022-04-30 22:03:22 -0500

    refactor: changes required by the dependencies update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,37 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const { fontFamily } = require('tailwindcss/defaultTheme')
 
 module.exports = {
-    future: {
-        removeDeprecatedGapUtilities: true,
+  content: ['./**/*.html'],
+  theme: {
+    screens: {
+      xs: '360px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
     },
-    experimental: {
-        applyComplexClasses: true,
+    container: {
+      center: true,
+      padding: '2rem'
     },
-    purge: ['./**/*.html'],
-    theme: {
-        screens: {
-            xs: '360px',
-            sm: '640px',
-            md: '768px',
-            lg: '1024px',
-            xl: '1280px',
-        },
-        container: {
-            center: true,
-            padding: '2rem'
-        },
-        extend: {
-            fontFamily: {
-                manrope: ['Manrope', ...defaultTheme.fontFamily.mono],
-            },
-            colors: {
-                'dark-gray-blue': 'hsl(217, 19%, 35%)',
-                'desa-dark-blue': 'hsl(214, 17%, 51%)',
-                'gray-blue': 'hsl(212, 23%, 69%)',
-                'light-gray-blue': 'hsl(210, 46%, 95%)',
+    extend: {
+      fontFamily: {
+        manrope: ['Manrope', ...fontFamily.mono],
+      },
+      colors: {
+        'dark-gray-blue': 'hsl(217, 19%, 35%)',
+        'desa-dark-blue': 'hsl(214, 17%, 51%)',
+        'gray-blue': 'hsl(212, 23%, 69%)',
+        'light-gray-blue': 'hsl(210, 46%, 95%)',
 
-                'erian-color': '#02AAB0',
-                'erian-hover': '#00CDAC'
-            },
-            borderRadius: {
-                xl: '1rem',
-            }
-        },
+        'erian-color': '#02AAB0',
+        'erian-hover': '#00CDAC'
+      },
+      borderRadius: {
+        xl: '1rem',
+      }
     },
-    variants: {},
-    plugins: []
+  },
+  variants: {},
+  plugins: []
 }
